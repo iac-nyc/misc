ad66573664a56cf31d4b9134694aae1cd318a4db
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-04-10 15:55:05 -0400

    hello world php

---
+++ php/project_1/inc/hello.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+ * This file is first "Hello World!" script
+ * 
+ * @author: Iftekhar A Chowdhury
+*/
+//first php script
+echo 'Hello World!'; //done
+
+
+//you can make a single line comment as long as you want, even wrapping to a new line, as long as you don't add a  hard return
+//This is a second comment line
+
+/*
+line 1
+line 2
+line 3
+*/
+?>
