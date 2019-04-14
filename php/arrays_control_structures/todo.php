bdaafa3f848b4bcc9946df721099a0931b3b735f
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-04-14 12:16:12 -0400

    todo

---
+++ php/arrays_control_structures/todo.php
@@ -0,0 +1,16 @@
+<?php
+$list[] = [
+    'title' => 'Create Mobile App',
+    'priority' => 2,
+    'due' => '',
+    'complete' => true,
+];
+$list[] = [
+    'title' => 'Clean out Desktop',
+    'priority' => 3,
+    'due' => '07/30/2016',
+    'complete' => false,
+];
+var_dump($list);
+//echo $list[0]['title'];
+?>
