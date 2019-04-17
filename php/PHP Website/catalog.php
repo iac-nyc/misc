7832ba047a6f623047e451fc98a6876d09bbd2fd
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-04-17 12:45:22 -0400

    catalog- function updated

--- php/PHP Website/catalog.php
+++ php/PHP Website/catalog.php
@@ -1,6 +1,7 @@
 <?php 
 
 include("inc/data.php");
+include("inc/functions.php");
 
 $pageTitle = "Full Catalog";
 $section = null;
@@ -28,13 +29,8 @@ include("inc/header.php"); ?>
         <h1><?php echo $pageTitle; ?> </h1>
         <ul class="items">
             <?php 
-                foreach($catalog as $item){
-                echo "<li><a href='#'>
-                <img src=' "
-                    .$item["img"]."' alt='"
-                    .$item["title"] ."' />"
-                    ."<p>View Details</p>"
-                    ."</a></li>";
+                foreach($catalog as $id => $item){
+                echo get_item_html($id,$item);
             }
             ?>
         </ul>
