a09bda42af90c130dbf833e6e1a609a3eada6aa5
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-04-18 10:14:10 -0400

    Displaying Categories

--- php/PHP Website/catalog.php
+++ php/PHP Website/catalog.php
@@ -28,9 +28,11 @@ include("inc/header.php"); ?>
     <div class="wrapper">
         <h1><?php echo $pageTitle; ?> </h1>
         <ul class="items">
-            <?php 
-                foreach($catalog as $id => $item){
-                echo get_item_html($id,$item);
+            <?php              
+
+            $categories = array_category($catalog, $section);
+                foreach($categories  as $id){
+                echo get_item_html($id,$catalog[$id]);
             }
             ?>
         </ul>
--- php/PHP Website/index.php
+++ php/PHP Website/index.php
@@ -1,7 +1,6 @@
 <?php 
 include("inc/data.php");
 include("inc/functions.php");
-
 $pageTitle = "Personal Media Library";
 $section = null;
 include("inc/header.php");?>
@@ -13,11 +12,11 @@ include("inc/header.php");?>
 
 				<ul class="items">
 			     <?php 
-                    $random = array_rand($catalog, 4);
-                    foreach($random  as $id){
-                        echo get_item_html($id,$catalog[$id]);
-                    }
-                ?>								
+                $random = array_rand($catalog, 4);
+                foreach($random  as $id){
+                echo get_item_html($id,$catalog[$id]);
+            }
+            ?>								
 				</ul>
 
 			</div>
