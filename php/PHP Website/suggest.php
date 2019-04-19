1042f63f837b7df1dbdd43fb3c748085ffdc1e35
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-04-19 17:56:20 -0400

    adding Form

--- php/PHP Website/suggest.php
+++ php/PHP Website/suggest.php
@@ -1,9 +1,34 @@
 <?php 
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
-include("inc/header.php");?>
+
+include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Suggest a Media Item</h1>
+    <div class="wrapper">
+       <h1>Suggest a Media Item</h1>
+        <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+        <form method="post">
+            <table>
+                <tr>
+                    <th><label for="name">Name</label></th>
+                    <td>  <input type="text" id="name" name="name" placeholder="Your Name"></td>
+                </tr>
+                  <tr>
+                    <th><label for="email">Email</label></th>
+                    <td>  <input type="text" id="email" name="email" placeholder="youremail@email.com"></td>
+                </tr>
+                 <tr>
+                    <th><label for="details">Suggest Item Detail</label></th>
+                    <td><textarea id="details" name="details"></textarea></td>
+                </tr>
+            </table>
+            <input type="submit" value="Send">
+          
+            
+        </form>
+    </div>
+  
 </div>
-<?php include("inc/footer.php");?>
+
+<?php include("inc/footer.php"); ?> <td></td>
