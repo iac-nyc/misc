87e9c2c45cd25e6e85e68fdad43cfa8437db0da0
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-04-21 11:47:02 -0400

    variable and handling form process from same file

--- php/PHP Website/suggest.php
+++ php/PHP Website/suggest.php
@@ -1,4 +1,24 @@
 <?php 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $details = $_POST["details"];
+    
+    echo "<pre>";
+    $email_body = "";   
+    
+    $email_body .= "Name " . $name . "\n";
+    $email_body .= "Email " . $email . "\n";
+    $email_body .= "Details " . $details . "\n";
+    
+    echo $email_body;
+    
+    echo "</pre>";
+    
+    // To Do: Send email
+    header("location:suggest.php?status=thanks");
+}  
+
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
@@ -7,26 +27,35 @@ include("inc/header.php"); ?>
 <div class="section page">
     <div class="wrapper">
        <h1>Suggest a Media Item</h1>
+        
+        <?php if(isset($_GET["status"]) && $_GET["status"] == "thanks"){   
+    
+            echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
+    
+        } else { ?>
+        
         <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
-        <form method="post" action="process.php">
-            <table>
-                <tr>
-                    <th><label for="name">Name</label></th>
-                    <td>  <input type="text" id="name" name="name" placeholder="Your Name"></td>
-                </tr>
-                  <tr>
-                    <th><label for="email">Email</label></th>
-                    <td>  <input type="text" id="email" name="email" placeholder="youremail@email.com"></td>
-                </tr>
-                 <tr>
-                    <th><label for="details">Suggest Item Detail</label></th>
-                    <td><textarea id="details" name="details"></textarea></td>
-                </tr>
-            </table>
-            <input type="submit" value="Send">
-          
-            
-        </form>
+        
+            <form method="post" action="suggest.php">
+                <table>
+                    <tr>
+                        <th><label for="name">Name</label></th>
+                        <td>  <input type="text" id="name" name="name" placeholder="Your Name"></td>
+                    </tr>
+                      <tr>
+                        <th><label for="email">Email</label></th>
+                        <td>  <input type="text" id="email" name="email" placeholder="youremail@email.com"></td>
+                    </tr>
+                     <tr>
+                        <th><label for="details">Suggest Item Detail</label></th>
+                        <td><textarea id="details" name="details"></textarea></td>
+                    </tr>
+                </table>
+                <input type="submit" value="Send">            
+            </form>
+        
+        <?php } ?>
+        
     </div>
   
 </div>
