ff5d5d654f650ea9aef967ca894ccbadaa7cfdbc
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-04-22 17:15:44 -0400

    Displaying error message

--- php/PHP Website/suggest.php
+++ php/PHP Website/suggest.php
@@ -15,53 +15,61 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $details = trim(filter_input(INPUT_POST,"details",FILTER_SANITIZE_SPECIAL_CHARS));
     
     if($name == "" || $email == "" || $category == "" || $title == ""){
-        echo "Please fill in the required fields: Name, Email, Category and Title";
-        exit;
+        $error_message = "Please fill in the required fields: Name, Email, Category and Title";
+        
     }
     
     if($_POST["address"] != ""){
-        echo "Bad form input";
-        exit;
+       $error_message = "Bad form input";
+      
     }
    if(!PHPMailer::validateAddress($email)){
-    echo "Invalid Email Address";
-       exit;
+    $error_message = "Invalid Email Address";
+      
    }
+    if(!isset($error_message)){
     
-    $email_body = "";
-    $email_body .= "Name " . $name . "\n";
-    $email_body .= "Email " . $email . "\n";
-    $email_body .= "\n\nSuggested Item\n\n";
-    $email_body .= "Category " . $category . "\n";
-    $email_body .= "Title " . $title . "\n";
-    $email_body .= "Format " . $format . "\n";
-    $email_body .= "Genre " . $genre . "\n";
-    $email_body .= "Year " . $year . "\n";
-    $email_body .= "Details " . $details . "\n";    
-
-    // To Do: Send email
-    $mail = new PHPMailer;
-   
-    //It's important not to use the submitter's address as the from address as it's forgery,
-    //which will cause your messages to fail SPF checks.
-    //Use an address in your own domain as the from address, put the submitter's address in a reply-to
-    $mail->setFrom('iftekhar@ilovechittagong.com', $name);
-    $mail->addAddress('iftekhar@ilovechittagong.com', 'Iftekhar Chowdhury');
-    $mail->addReplyTo($email, $name);
-    $mail->Subject = 'Library Suggestion from ' . $name;
-    $mail->Body = $email_body;
-    if (!$mail->send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-        exit;
+        $email_body = "";
+        $email_body .= "Name " . $name . "\n";
+        $email_body .= "Email " . $email . "\n";
+        $email_body .= "\n\nSuggested Item\n\n";
+        $email_body .= "Category " . $category . "\n";
+        $email_body .= "Title " . $title . "\n";
+        $email_body .= "Format " . $format . "\n";
+        $email_body .= "Genre " . $genre . "\n";
+        $email_body .= "Year " . $year . "\n";
+        $email_body .= "Details " . $details . "\n";    
+    
+        // To Do: Send email
+        $mail = new PHPMailer;
+       
+        //It's important not to use the submitter's address as the from address as it's forgery,
+        //which will cause your messages to fail SPF checks.
+        //Use an address in your own domain as the from address, put the submitter's address in a reply-to
+        $mail->setFrom('iftekhar@ilovechittagong.com', $name);
+        $mail->addAddress('iftekhar@ilovechittagong.com', 'Iftekhar Chowdhury');
+        $mail->addReplyTo($email, $name);
+        $mail->Subject = 'Library Suggestion from ' . $name;
+        $mail->Body = $email_body;
+        if ($mail->send()) {
+            header("location:suggest.php?status=thanks");
+            exit;
+            
+            } 
+           $error_message = "Mailer Error: " . $mail->ErrorInfo;
+            
+       
+        
     }
-    header("location:suggest.php?status=thanks");
 }  
 
 $pageTitle = "Suggest a Media Item";
 $section = "suggest";
 
-include("inc/header.php"); ?>
+include("inc/header.php"); 
 
+
+?>
 <div class="section page">
     <div class="wrapper">
        <h1>Suggest a Media Item</h1>
@@ -70,9 +78,15 @@ include("inc/header.php"); ?>
     
             echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
     
-        } else { ?>
+        } else {
+            if(isset($error_message)){
+                echo '<p class="message">' .$error_message.'</p>';    
+            } else {
+                 echo '<p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>';
+            }
+        ?>
         
-        <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+       
         
             <form method="post" action="suggest.php">
                 <table>
