6fa2272273b20a2085592957b2df7557f259e4e8
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-05-07 10:31:16 -0400

    Matching Search Patterns

--- php/PHP Website/catalog.php
+++ php/PHP Website/catalog.php
@@ -5,6 +5,7 @@ include("inc/functions.php");
 
 $pageTitle = "Full Catalog";
 $section = null;
+$search = null;
 $items_per_page = 8;
 
 if (isset($_GET["cat"])) {
@@ -19,9 +20,14 @@ if (isset($_GET["cat"])) {
         $section = "music";
     }
 }
+if (isset($_GET["s"])) {
+  $search = filter_input(INPUT_GET,"s",FILTER_SANITIZE_STRING);
+}
+
 if (isset($_GET["pg"])) {
   $current_page = filter_input(INPUT_GET,"pg",FILTER_SANITIZE_NUMBER_INT);
 }
+
 if (empty($current_page)) {
   $current_page = 1;
 }
