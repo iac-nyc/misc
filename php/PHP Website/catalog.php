a1e4c7fb1851f07e54b519fca07b233f42d8acdf
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-05-08 09:59:13 -0400

    accounting for empty results

--- php/PHP Website/catalog.php
+++ php/PHP Website/catalog.php
@@ -2,7 +2,6 @@
 
 include("inc/functions.php");
 
-
 $pageTitle = "Full Catalog";
 $section = null;
 $search = null;
@@ -33,38 +32,34 @@ if (empty($current_page)) {
 }
 
 $total_items = get_catalog_count($section, $search);
-$total_pages = ceil($total_items / $items_per_page);
+$total_pages = 1;
+$offset = 0;
+if($total_items > 0){
+    $total_pages = ceil($total_items / $items_per_page);
 
-//limit results in redirect
-$limit_results = "";
-if (!empty($section)) {
-    $limit_results = "cat=" . $section . "&";
-}
-
-// redirect too-large page numbers to the last page
-if ($current_page > $total_pages) {
-    header("location:catalog.php?" 
-        . $limit_results 
-        . "pg=".$total_pages);
-}
-// redirect too-small page numbers to the first page
-if ($current_page < 1) {
-    header("location:catalog.php?"
-        . $limit_results
-        . "pg=1");
-}
-
-//determine the offset (number of items to skip) for the current page
-//for example: on page 3 with 8 item per page, the offset would be 16
-$offset = ($current_page - 1) * $items_per_page;
-
-if(!empty($search)){
-    $catalog = search_catalog_array($search,$items_per_page, $offset);
-} else if(empty($section)){
-    $catalog = full_catalog_array();
-} else {
-    $catalog = category_catalog_array($section);
-}
+    //limit results in redirect
+    $limit_results = "";
+    if (!empty($section)) {
+        $limit_results = "cat=" . $section . "&";
+    }
+    
+    // redirect too-large page numbers to the last page
+    if ($current_page > $total_pages) {
+        header("location:catalog.php?" 
+            . $limit_results 
+            . "pg=".$total_pages);
+    }
+    // redirect too-small page numbers to the first page
+    if ($current_page < 1) {
+        header("location:catalog.php?"
+            . $limit_results
+            . "pg=1");
+    }
+    
+    //determine the offset (number of items to skip) for the current page
+    //for example: on page 3 with 8 item per page, the offset would be 16
+    $offset = ($current_page - 1) * $items_per_page;
+    
     $pagination = "<div class=\"pagination\">";
     $pagination .= "Pages: ";  
     for ($i = 1;$i <= $total_pages;$i++) {
@@ -81,6 +76,16 @@ if(!empty($search)){
         }
     }
     $pagination .= "</div>";
+}
+
+if(!empty($search)){
+    $catalog = search_catalog_array($search,$items_per_page, $offset);
+} else if(empty($section)){
+    $catalog = full_catalog_array();
+} else {
+    $catalog = category_catalog_array($section);
+}
+    
 
 include("inc/header.php"); ?>
 
@@ -89,20 +94,33 @@ include("inc/header.php"); ?>
     <div class="wrapper">
         
         <h1><?php 
+        if($search != null){
+            echo "Search Results for \" ".htmlspecialchars($search)."\"";
+        } else {    
         if ($section != null) {
             echo "<a href='catalog.php'>Full Catalog</a> &gt; ";
         }
-        echo $pageTitle; ?></h1>
+        echo $pageTitle;
+        }
+            ?></h1>
         
-        <?php echo $pagination; ?>      
-        <ul class="items">
-            <?php
-              foreach ($categories as $item) {
-                echo get_item_html($item);
-            }
-            ?>
-        </ul>
-         <?php echo $pagination; ?>
+        <?php 
+        if($total_items < 1 ){
+            echo "<p>No items were found matching that search term.</p>";
+            echo "<p>Search again or "."<a href=\"catalog.php\"> Browse the Full Catalog</a></p>";
+        
+        } else {
+             echo $pagination; ?>      
+            <ul class="items">
+                <?php
+                  foreach ($categories as $item) {
+                    echo get_item_html($item);
+                }
+                ?>
+            </ul>
+         <?php echo $pagination; 
+        }
+        ?>
         
     </div>
 </div>
