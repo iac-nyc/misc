77f98af6c404381cc82e04c48ebed901fbff24a0
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-05-11 11:22:49 -0400

    cookies

--- php/story_game/index.php
+++ php/story_game/index.php
@@ -1,10 +1,15 @@
 <?php
 include 'inc/header.php';
 
-echo '<h1>Welcome to our<br />Story Game</h1>';
+echo '<h1>Welcome to our<br />Treehouse Story Game</h1>';
 echo '<p>Enter the requested words and create your story.</p>';
 echo '<p><a class="btn btn-default btn-lg" href="play.php" role="button">Play</a></p>';
 
 echo '<h2>Reread Your Saved Stories</h2>';
+if(isset($_COOKIE)){
+    foreach($_COOKIE as $key => $value){
+        echo $key . '<br />';
+    }
+}
 
 include 'inc/footer.php';
