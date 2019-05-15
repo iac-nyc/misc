ce575d544569d429f6c12d0f940992179327f698
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-05-15 09:16:47 -0400

    filtering by project

--- php/time_tracker/inc/functions.php
+++ php/time_tracker/inc/functions.php
@@ -16,6 +16,13 @@ function get_task_list($filter = null){
     include 'connection.php';
     $sql = 'SELECT tasks.*, projects.title as project FROM tasks JOIN projects ON tasks.project_id = projects.project_id';
     
+    $where='';
+    if(is_array($filter)){
+        if($filter[0] == 'project'){
+            $where = ' WHERE projects.project_id = ?';
+        }
+    }
+    
     $orderBy = ' ORDER BY date DESC';
     
     if($filter) {
@@ -23,7 +30,10 @@ function get_task_list($filter = null){
     }
     
     try{
-     $results =  $db->prepare($sql . $orderBy);
+     $results =  $db->prepare($sql . $where . $orderBy);
+        if(is_array($filter)) {
+            $results->bindValue(1,$filter[1], PDO::PARAM_INT);
+        }
      $results->execute();   
     } catch (Exception $e){
         echo "Error! : ". $e->getMessage(). "</br>";
