c48a547154a8cc5b130efd9c2c8bbcf6b23eb599
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-05-17 10:44:23 -0400

    Writing Files Line by Line

---
+++ php/file_handling/combine.php
@@ -0,0 +1,19 @@
+<?php
+
+if($fh = fopen('data/html/combined.html', 'w')){
+    fwrite($fh, file_get_contents('data/html/states.html'));
+    
+    fwrite($fh, PHP_EOL.'<optgroup label="US Outlying Territories">');
+    fwrite($fh, PHP_EOL. file_get_contents('data/html/territories.html'));
+    fwrite($fh, PHP_EOL. '</optgroup>');
+    
+    fwrite($fh, PHP_EOL. '<optgroup label="Armed Forces">');
+    fwrite($fh, PHP_EOL. file_get_contents('data/html/armed_forces.html'));
+    fwrite($fh, PHP_EOL. '</optgroup>');
+    
+    fclose($fh);
+}
+
+echo '<select>';
+include 'data/html/combined.html';
+echo '</select>';
